f6a526d98c781c16453dc5465ecaf204305e05bf
Paul Verheul <paulverheul@MBP-van-Paul.lan>
2019-04-23 20:38:44 +0200

    Make sure assets are only loaded when they should be loaded

--- src/Plugin.php
+++ src/Plugin.php
@@ -29,17 +29,46 @@ class Plugin extends \craft\base\Plugin
         // Set instance
         self::$instance = $this;
 
-        // Load settings
         $settings = $this->getSettings();
-
         $request = Craft::$app->getRequest();
         $view = Craft::$app->getView();
 
         // Load JS, if admin request
-        if ($settings->pluginIsActive && $request->isCpRequest) {
+        if ($request->isCpRequest && $this->assetsShouldLoad()) {
             $view->registerJs('window.slugAlertMessage = "' . $settings->alertMessage . '";');
             $view->registerAssetBundle(SlugAlertJsAsset::class);
         }
+    }
+
+    protected function assetsShouldLoad()
+    {
+        $settings = $this->getSettings();
+
+        // Is the plugin active?
+        if (!$settings->pluginIsActive) {
+            return false;
+        }
+
+        // Use URL segments to determine what type of page we're on
+        $request = Craft::$app->getRequest();
+        $segments = $request->getSegments();
+        
+        $firstSegment = $segments[0] ?? null;
+        $thirdSegment = $segments[2] ?? null;
+
+        $isEntryEditPage = $firstSegment === 'entries' && !empty($thirdSegment);
+        $isCategoryEditPage = $firstSegment === 'categories' && !empty($thirdSegment);
+        $isNewEntryPage = $firstSegment === 'entries' && $thirdSegment === 'new';
+        $isNewCategoryPage = $firstSegment === 'categories' && $thirdSegment === 'new';
+
+        if (
+            ($isEntryEditPage && !$isNewEntryPage)
+            || ($isCategoryEditPage && $isNewCategoryPage)
+        ) {
+            return true;
+        }
+
+        return false;
     }
 
     protected function settingsHtml()
